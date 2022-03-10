12558d7c72ef6b90cd51028e01d422deb626dfd5
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-10 14:39:06 +0900

    初期値の文字を削除

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css';
 
 export const App = () => {
   const [todoText, setTodoText] = useState('');
-  const [incompleteTodos, setIncompleteTodos] = useState(['ああああ', 'いいいいい']);
-  const [completeTodos, setCompleteTodos] = useState(['ううううう']);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
