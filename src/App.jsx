f9f1995b57c5473eb1c88d23ccdc290d5e0a0e1f
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-10 12:45:01 +0900

    タスクの削除機能

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,18 @@ export const App = () => {
   const [completeTodos, setCompleteTodos] = useState(['ううううう']);
 
   const onChangeTodoText = (event) => setTodoText(event.target.value);
+
   const onClickAdd = () => {
     if (todoText === '') return;
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
     setTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    setIncompleteTodos(newTodos);
+    newTodos.splice(index, 1);
   };
 
   return (
@@ -23,12 +30,12 @@ export const App = () => {
       <div className='incomplete-area'>
         <p className='title'>未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className='list-row'>
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             )
           })}
