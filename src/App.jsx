091267403b20e969aee450db7bc820dff4085866
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-10 11:44:54 +0900

    htmlのマークアップ

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 export const App = () => {
-  return <p>test</p>
-}
+  return (
+    <>
+      <div>
+        <input type="text" placeholder='TODOを入力'/>
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>ああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>いいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>ああああ</li>
+            <button>戻す</button>
+          </div>
+          <div>
+            <li>いいい</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
+};
