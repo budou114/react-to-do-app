e90de5be5e17b2660ec4adc60741b8f33f50adc9
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-10 14:57:25 +0900

    新規タスク追加機能をコンポーネント化

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import { InputTodo } from './components/InputTodo'; 
 
 export const App = () => {
   const [todoText, setTodoText] = useState('');
@@ -41,10 +42,11 @@ export const App = () => {
 
   return (
     <>
-      <div className='input-area'>
-        <input type="text" placeholder='TODOを入力' value={todoText} onChange={onChangeTodoText}/>
-        <button onClick={onClickAdd}>追加</button>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
       <div className='incomplete-area'>
         <p className='title'>未完了のTODO</p>
         <ul>
